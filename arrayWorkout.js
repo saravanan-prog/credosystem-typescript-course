963fbb7c55890b3a8a8a48156f89774b20203990
saravanan-prog <saravanan.mca1992@gmail.com>
2026-06-26 09:27:03 +0530

    workout

---
+++ arrayWorkout.js
@@ -0,0 +1,7 @@
+"use strict";
+var studentNameList = ["kesvan", "madhavan", "vimalan"];
+var princeList = [1000, 25, 85, 92, 45];
+var checkout = ["dholl", "rice", "mango", "banana"];
+var [studnet1, student2] = studentNameList; // de strcuture
+console.log("studnet 1 ====>", studnet1);
+console.log("studnet 2 ====>", student2);
---
+++ arrayWorkout.ts
@@ -0,0 +1,24 @@
+// Example - 1
+
+var studentNameList:string[]    = ["kesvan","madhavan","vimalan"]
+var princeList:number[]         = [1000,25,85,92,45]
+var checkout:string[]           = ["dholl","rice","mango","banana"]
+
+var studentsMark:(string | number) [] = ["saravanan",100]
+
+
+
+var [studnet1,student2] = studentNameList   // de strcuture
+
+console.log("studnet 1 ====>",studnet1)
+console.log("studnet 2 ====>",student2)
+
+
+// Example 2
+
+var schoolTypes : Array <string> = ["CBSE","Matriculation","state Board","centeralbord"]
+var classRoom : Array <string> = ["Ac-room","Non-Ac","Auditorium"]
+var generalStore : Array<string | number> = ["dholl",2,"mongdhall",10,""]
+
+
+
