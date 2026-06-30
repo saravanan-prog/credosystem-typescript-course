f18db6cae8c1f54d2a0b6a0d2ab1731c4ff0417d
saravanan-prog <saravanan.mca1992@gmail.com>
2026-07-01 00:27:17 +0530

    workout

--- type-example.js
+++ type-example.js
@@ -1,10 +1,11 @@
 "use strict";
-let candidateAge = ["25"];
-console.log("CanidateAge====>", candidateAge);
-var student = {
+let candidateAge = 25;
+console.log("CandidateAge ===>", candidateAge);
+let student = {
     student_name: "Ravi",
     student_age: 25,
     student_course: "React",
     tamil: 50,
     english: 75
 };
+console.log(student);
--- type-example.ts
+++ type-example.ts
@@ -1,28 +1,29 @@
-type CandidateAge = string | number
-let candidateAge:CandidateAge = 25
-console.log("CanidateAge====>",candidateAge)
+type CandidateAge = string | number;
 
+let candidateAge: CandidateAge = 25;
 
-
+console.log("CandidateAge ===>", candidateAge);
 
 type BasicDetails = {
-    student_name:string,
-    student_age:number,
-    student_course:string
-}
+    student_name: string;
+    student_age: number;
+    student_course: string;
+};
 
-type StudetMarks = {
-    tamil:number,
-    english:number
-}
+type StudentMarks = {
+    tamil: number;
+    english: number;
+};
 
-type Student = BasicDetails & StudetMarks
+// Intersection Type
+type Student = BasicDetails & StudentMarks;
 
-var student:Student = {
-    student_name:"Ravi",
-    student_age:25,
-    student_course:"React",
-    tamil:50,
-    english:75
-   
-}
+let student: Student = {
+    student_name: "Ravi",
+    student_age: 25,
+    student_course: "React",
+    tamil: 50,
+    english: 75
+};
+
+console.log(student);
